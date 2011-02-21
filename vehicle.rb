1ff4c48e43bbf202f80b3ddfdec2075195749318
Philip Hale <p.hale.09@aberdeen.ac.uk>
2011-02-21 14:54:32 +0000

    end of practical

--- vehicle.rb
+++ vehicle.rb
@@ -1,21 +1,26 @@
 class Vehicle
-  attr_accessor :name
-  attr_accessor :travel_options
-  attr_accessor :travel_mode
-  attr_accessor :sound_options
+  attr_accessor :name, :travel_options, :travel_mode, :sound_options, :list
   
-  def initialize(n="abc")
+  def initialize(n="george")
     @travel_options = ["fly", "drive", "float", "hover"]
     @sound_options = ["whoosh", "vroom", "swish", "brrrr"]
-    @name = n
+    @@list = Array.new    
+    list.push(@name = n)
+
   end  
   
   def travel_choice(choice=0)
     @travel_mode = travel_options[choice-1]
+    list.push(travel_mode)
   end
   
   def sound(choice=0)
     @sound = sound_options[choice]
   end
   
+  def list
+    return @@list
+  end
 end
+
+
